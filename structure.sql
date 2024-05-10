b14a33224ca57fc2da691724ae2f587667fa1c34
thenamdev <ntnhacker1@gmail.com>
2024-05-10 13:56:51 +0700

    fix

--- structure.sql
+++ structure.sql
@@ -4,12 +4,11 @@ CREATE TABLE Courses (
 );
 
 CREATE TABLE CourseComponents (
-    component_id INT PRIMARY KEY AUTO_INCREMENT,
-    course_id INT,
-    component_sequence INT NOT NULL,
+    component_id INT NOT NULL,
+    course_id INT NOT NULL,
     component_name VARCHAR(50) NOT NULL,
-    FOREIGN KEY (course_id) REFERENCES Courses(course_id),
-    UNIQUE KEY (course_id, component_sequence)
+    PRIMARY KEY (component_id, course_id),
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
 );
 
 CREATE TABLE Students (
@@ -28,12 +27,11 @@ CREATE TABLE ComponentStudents (
     FOREIGN KEY (course_id) REFERENCES Courses(course_id)
 );
 CREATE TABLE Attendance (
-    component_id INT,
-    student_id INT,
+    course_id INT NOT NULL,
+    component_id INT NOT NULL,
+    student_id INT NOT NULL,
     attendance_date DATE NOT NULL,
-    course_id INT,
-    FOREIGN KEY (component_id) REFERENCES CourseComponents(component_id),
-    FOREIGN KEY (student_id) REFERENCES Students(student_id),
-    FOREIGN KEY (course_id) REFERENCES Courses(course_id),
-    PRIMARY KEY (course_id, component_id, student_id, attendance_date)
+    PRIMARY KEY (course_id, component_id, student_id, attendance_date),
+    FOREIGN KEY (component_id, course_id) REFERENCES CourseComponents(component_id, course_id),
+    FOREIGN KEY (student_id) REFERENCES Students(student_id)
 );
