baf852a0e188153dbb9de1a5576f68f587ddee6e
thenamdev <ntnhacker1@gmail.com>
2024-05-11 00:41:52 +0700

    up

--- getData.sql
+++ getData.sql
@@ -10,8 +10,7 @@ INSERT INTO Attendance (course_id, component_id, student_id, attendance_date)
 VALUES 
 (1, 1, 1, '2024-05-10'),
 (1, 1, 2, '2024-05-10'),
-(2, 1, 1, '2024-05-10'),
-(1, 1, 3, '2024-05-15');
+(2, 1, 1, '2024-05-10');
 
 -- lấy thông tin ai đã điểm danh
 SELECT
