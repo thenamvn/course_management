bce8aa576e3f8594196f3d04ea34f4c27aa843d6
thenamdev <ntnhacker1@gmail.com>
2024-05-12 00:51:57 +0700

    ??

---
+++ admin/js/bootstrap.js
@@ -0,0 +1,6 @@
+document.getElementById("togglePassword").addEventListener("click", function(e) {
+    const passwordInput = document.getElementById("password");
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
+    this.classList.toggle("fa-eye-slash");
+});
